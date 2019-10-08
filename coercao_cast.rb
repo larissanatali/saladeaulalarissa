695f14ea2b78ba3fb56b88e0db98c65245fa2dce
larissa Natali <larissa.natali00@gmail.com>
2019-10-07 21:47:20 -0300

    coercao_cast

---
+++ coercao_cast.rb
@@ -0,0 +1,17 @@
+=begin
+ 
+Serve para transformar uma variavel para inteiro, 
+Float ou String, Array ou Simbolo. 
+
+TO_I - Converte a variavel interiro. 
+TO_F - Converte a variavel float.
+TO_S - Converte a variavel para string.
+TO_A - Converte a variavel para array. 
+TO_SYM - Converte a variavel para simbolo. 
+    
+=end
+
+variavel = '1'
+variavel2 = 2
+
+puts variavel.to_i + 2 
