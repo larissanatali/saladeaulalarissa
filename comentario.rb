2f6a4df1d79b49013659426dc539d3849aab3df0
larissa Natali <larissa.natali00@gmail.com>
2019-10-07 21:25:28 -0300

    Comentario

---
+++ comentario.rb
@@ -0,0 +1,9 @@
+#Fazer comentario de uma linha 
+
+=begin comeco do comentario
+Comentario varias linhas 
+teste teste teste 
+teste teste teste
+=end final do comentario
+teste teste teste 
+teste teste teste 
