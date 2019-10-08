39599b5994e32e471a7f0439d837dece2c8d34cc
larissa Natali <larissa.natali00@gmail.com>
2019-10-07 22:05:43 -0300

    operadores_aritmeticos

---
+++ Aula_1/interpolacao.rb
@@ -0,0 +1,14 @@
+# Juntar caracteres com inteiro 
+# Contaquenar variaveis 
+
+Variavel = 'Larissa'
+
+puts 'ola ' + Variavel
+
+puts 'ola ' << Variavel
+
+variavel_inteira = 2 
+variavel_string = 'total e '
+
+#Contaquenar duas variaveis de tipos diferentes 
+puts "#{variavel_string}" + "#{variavel_inteira}"  
---
+++ Aula_1/operadores_aritmeticos.rb
@@ -0,0 +1,17 @@
+=begin
+Opperadores: 
+
++ Adicao
+- Subtracao
+* Multiplicacao
+/ Divisao
+% Modulo(Pega o resto da divisao)
+** Exponenciacao (2 elevado a 2)
+=end
+
+puts 2 + 2
+puts 2 - 2
+puts 2 * 2
+puts 10 / 2
+puts 10 % 2
+puts 2 ** 2
