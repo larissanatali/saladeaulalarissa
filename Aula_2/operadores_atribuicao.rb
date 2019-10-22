99040b76acf91c1a3e60ce4aa9614fd723b882a0
larissa silva <larissanatalii@MacBook-Air-de-larissa.local>
2019-10-22 07:21:10 -0300

    Sala

--- Aula_2/operadores_atribuicao.rb
+++ Aula_2/operadores_atribuicao.rb
@@ -1,18 +1,30 @@
 #Quando queremos atribuir um valor a mais na mesmoa variavel e usado desde jeito: 
 
-puts A = 4 
 # Atribui um valor a uma variavel.
-puts A = A + 4 
+A = 4
+A = A + 1
+puts A
 #Equivale A = A + 4
-puts A += 4 
+B = 2
+A += 4 
+puts A
 #Equivale A = A - 4
-puts A -= 4 
+C = 9 
+C -= 4 
+puts C 
 #Equivale A = A * 4
-puts A *= 4 
+D = 7
+D *= 4 
+puts D 
 #Equivale A = A % 4
-puts A %= 4 
+E = 5
+E %= 4
+puts E  
 #Equivale A = A / 4
-puts A /= 4 
+F = 3
+F /= 4 
+puts F
 #Equivale A = A ** 4
-puts A **= 4 
-
+G = 1
+G **= 4 
+puts G 
